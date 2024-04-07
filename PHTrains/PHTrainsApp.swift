0f2da87db60364fca93a3eea790174bfab4b352b
John Lee <johnlee.programming@gmail.com>
2024-04-07 13:33:40 +0800

    Set up project

---
+++ PHTrains/PHTrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHTrainsApp.swift
+//  PHTrains
+//
+//  Created by John Lee on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PHTrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
